071cca4abf89f4b07b2ba3ce530757d26ae96c10
LIUXIAOBO-SHUAI\Administrator <lxb>
2021-03-31 21:54:21 +0800

    提出公共部分2

--- dormitory/src/main/java/com/sanxiau/dormitory/liu/controller/LiuController.java
+++ dormitory/src/main/java/com/sanxiau/dormitory/liu/controller/LiuController.java
@@ -39,40 +39,5 @@ public class LiuController {
         return "liu/success";
     }
 
-    //主页面
-    @RequestMapping("/index")
-    public String index(Model model){
-        //10条公告
-        List<Notice> notices = new ArrayList<>();
-        notices = noticeDao.findTen();
-        model.addAttribute("notices",notices);
-
-        //消息
-        List<Electric> news1 = new ArrayList<>();
-        Electric news = new Electric();
-            //根据寝室号查询电费消息
-            //假数据
-            String dorId = "102";
-            news1 = electricDao.findByDorId(dorId);
-            //如果没有需要缴费信息
-            if (news1 == null || news1.isEmpty()){
-                news.setId(1);
-                news.setPayMonth("还未更新");
-                news.setHouId("...");
-                news.setDorId("...");
-                news.setDegrees(0.00);
-                news.setPrice(BigDecimal.valueOf(0.00));
-                news.setAllPrice(BigDecimal.valueOf(0.00));
-                news.setConductor("...");
-                news.setPayState("...");
-                model.addAttribute("news", news);
-            }else {
-                news = news1.get(0);
-                model.addAttribute("news", news);
-            }
-
-
-        return "comm/index";
-    }
 
 }
